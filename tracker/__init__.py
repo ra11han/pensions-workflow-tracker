db2d73c74d0e7ec4ad038b1056ded2cdb8cf08f5
Rahan Hussain <rahan.hussain@capita.com>
2026-04-08 02:04:18 +0100

    Create superuser via post-migrate signal (Render free tier)

--- tracker/__init__.py
+++ tracker/__init__.py
@@ -1,1 +1,3 @@
 """Tracker app package containing workflow case management logic."""
+
+default_app_config = "pensions_tracker.apps.PensionsTrackerConfig"
--- tracker/apps.py
+++ tracker/apps.py
@@ -1,17 +1,41 @@
 """App configuration and post-migrate seeding for tracker data."""
 
-import sys
+import sys, os
 
 from django.apps import AppConfig
 from django.conf import settings
 from django.db.models.signals import post_migrate
 from django.dispatch import receiver
+from django.contrib.auth import get_user_model
+
+
+
+def create_superuser(sender, **kwargs):
+    username = os.getenv("DJANGO_SUPERUSER_USERNAME")
+    email = os.getenv("DJANGO_SUPERUSER_EMAIL")
+    password = os.getenv("DJANGO_SUPERUSER_PASSWORD")
+
+    if not username or not password:
+        return
+
+    User = get_user_model()
+
+    if not User.objects.filter(username=username).exists():
+        User.objects.create_superuser(
+            username=username,
+            email=email,
+            password=password
+        )
 
 
 class TrackerConfig(AppConfig):
     """Registers metadata for the tracker Django app."""
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'tracker'
+
+def ready(self):
+        post_migrate.connect(create_superuser, sender=self)
+
 
 
 @receiver(post_migrate)
